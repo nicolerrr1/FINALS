c54fa47d5e464e18a8f86ec264651078346cc638
nicolerrr1 <21-08560@g.batstate-u.edu.ph>
2024-05-16 10:59:03 +0800

    first draft

---
+++ orca_share_media1715128881989_7193771930245982140 (1).py
@@ -0,0 +1,20 @@
+class UserManager:
+	def load_users():
+		pass
+
+	def save_users():
+		pass
+
+	def validate_username():
+		pass
+
+	def validate_password():
+		pass
+
+	def register():
+		pass
+
+	def login():
+		pass
+
+	
---
+++ orca_share_media1715128881995_7193771930268482031.py
@@ -0,0 +1,58 @@
+import random
+
+class DiceGame:
+	def load_scores():
+		pass
+
+	def save_scores(self,points,stage):
+		f = open ("Ranking.txt","a")
+		f.write(f"username:  points-{points},  stages-{stage}")
+		f.close()
+
+	def play_game(self):
+		player_score = 0
+		bot_score = 0
+		round = 0
+		total = 0
+		while True:
+			while bot_score <2 and player_score<2:
+				bot_point = random.randint(1,6)
+				player_point = random.randint(1,6)
+				print(f"bot: {bot_point}")
+				print(f"player: {player_point}")
+				if bot_point == player_point:
+					print("Its a tie!")
+				if bot_point > player_point:
+					print("You lost, Username!")
+					bot_score += 1
+
+				if bot_point < player_point:
+					print("You won, Username!")
+					player_score += 1
+					total +=1
+
+			if bot_score< player_score:
+				print("You won the , Username!")
+				player_score+=3
+				total+=3
+				round+=1
+				self.save_scores(total, round)
+				break
+			if bot_score>player_score:
+				print("You lost the game, Username!")
+				break
+
+
+
+
+	def show_top_scores():
+		pass
+
+	def logout():
+		pass
+
+	def menu():
+		pass
+
+dice = DiceGame()
+dice.play_game()

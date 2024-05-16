55ccf1d2d4330d03f800186bc54a19e7f9681e7f
nicolerrr1 <21-08560@g.batstate-u.edu.ph>
2024-05-16 15:42:56 +0800

    draft may mali

--- final_lab/main.py
+++ final_lab/main.py
@@ -1,25 +1,19 @@
 from utils.dice_game import DiceGame
 
 def main():
-    # Create a DiceGame instance
     dice_game = DiceGame()
 
-    # Display the menu
     while True:
         print("Welcome to Dice Roll Game!")
         print("1. Register")
         print("2. Login")
         print("3. Exit")
-
+        
         choice = input("Enter your choice: ")
-
         if choice == '1':
-            # Call register method of DiceGame
-            dice_game.register()
+                dice_game.register()
         elif choice == '2':
-            # Call login method of DiceGame
             dice_game.login()
-            # If login successful, display the menu for the logged-in user
             if dice_game.current_user:
                 dice_game.menu()
         elif choice == '3':
